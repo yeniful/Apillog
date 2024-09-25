35ab8c9d88ebcda1b725a13e07bb9ddbd7ebf6e1
Yeni Hwang <yeniful@icloud.com>
2024-09-25 10:42:58 +0900

    Initial Commit

---
+++ ApillogTests/ApillogTests.swift
@@ -0,0 +1,16 @@
+//
+//  ApillogTests.swift
+//  ApillogTests
+//
+//  Created by yeni on 9/25/24.
+//
+
+import Testing
+
+struct ApillogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
