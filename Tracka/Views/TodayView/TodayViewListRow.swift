72ec8b0bf5833e0304be0f6f999ddab145f1a5e7
yeni <dev.yeeun@gmail.com>
2025-01-08 17:33:26 +0900

    Fix: Removed temp swift data codes

---
+++ Tracka/Views/TodayView/TodayViewListRow.swift
@@ -0,0 +1,38 @@
+//
+//  Row.swift
+//  Tracka
+//
+//  Created by yeni on 10/1/24.
+//
+
+import SwiftUI
+
+struct TodayViewListRow: View {
+    
+    @State var medication: PrimaryMedication
+    var isLastRow: Bool
+    
+    var body: some View{
+        VStack(alignment: .center, spacing: 0){
+            // Row
+            ZStack{
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(height: 80)
+                
+                // Row Content
+                HStack(alignment: .center){
+                    Image("PillGreen")
+                    VStack(alignment: .leading){
+                        Text(medication.name + " " + "\(medication.strength)" + medication.unit)
+                            .font(.title3)
+                            .foregroundColor(.descriptionBlack)
+                        Text("아직 복용하지 않음")
+                        .foregroundColor(.deactivatedGray)}
+                    Spacer()}}
+            
+            if !isLastRow {
+                RowDivider()}
+        }
+    }
+}
