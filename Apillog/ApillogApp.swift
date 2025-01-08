14a267c124fc68aec33669d7403b0b6ae5e6dfb1
yeni <dev.yeeun@gmail.com>
2025-01-08 17:23:57 +0900

    Fix: Removed temp swift data codes

--- Apillog/ApillogApp.swift
+++ Apillog/ApillogApp.swift
@@ -10,24 +10,24 @@ import SwiftData
 
 @main
 struct TrackaApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            PrimaryMedication.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+//    var sharedModelContainer: ModelContainer = {
+//        let schema = Schema([
+//            PrimaryMedication.self,
+//        ])
+//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+//
+//        do {
+//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+//        } catch {
+//            fatalError("Could not create ModelContainer: \(error)")
+//        }
+//    }()
 
     var body: some Scene {
         WindowGroup {
 //            ContentView()
             TodayView()
         }
-        .modelContainer(sharedModelContainer)
+//        .modelContainer(sharedModelContainer)
     }
 }
